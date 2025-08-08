f9fe6446f7dae5e297958e22b67af39216b39759
rohitghatal1 <rohitghatal@gmail.com>
2025-08-08 22:55:10 +0545

    added auth controller

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,3 +1,7 @@
 export const registerUser = async (req:any, res:any) => {
 
 }
+
+export const loginUser = async (req:any, res:any) => {
+    
+}
--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import { verifyToken } from '../middleware/authMiddleware';
-import { registerUser } from '../controllers/AuthController';
+import { loginUser, registerUser } from '../controllers/AuthController';
 
 const router = express.Router();
 
 router.post("/register", verifyToken, registerUser )
-
+router.post("/login", verifyToken, loginUser);
 
 
 export default router
