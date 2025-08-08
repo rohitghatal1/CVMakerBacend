4d6deb546e539e731620f931d1e5ee0413e2202b
rohitghatal1 <rohitghatal@gmail.com>
2025-08-08 22:54:22 +0545

    added auth controller

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import { verifyToken } from '../middleware/authMiddleware';
+import { registerUser } from '../controllers/AuthController';
 
 const router = express.Router();
 
-router.post("/register", verifyToken, )
+router.post("/register", verifyToken, registerUser )
 
 
 
