a8c8600ab0dcbaeb9df33517923f7f526a422bae
rohitghatal1 <rohitghatal@gmail.com>
2025-09-09 22:16:09 +0545

    added helmet

--- src/sever.ts
+++ src/sever.ts
@@ -17,21 +17,5 @@ app.use(cors());
 app.use(helmet());
 
 
-//Global error handler
-app.use(errorHandler);
-
-//Socket.IO connection listener
-io.on("connection", (socket:any) => {
-    console.log("new client connected: ", socket.id);
-
-    socket.on("disconnect", () => {
-        console.log("Client disconnected: ", socket.id);
-    })
-})
-
 // start the server
 const PORT = process.env.PORT || 5000;
-
-connectDB().then(() => {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-})
